c7c6c74089519e9f9358400889bd333a716efd4b
Kwan Jun Wen <junwenkwan97@gmail.com>
2020-06-06 20:20:41 +0800

    Add copyright statement on each go file

--- owsms/types_test.go
+++ owsms/types_test.go
@@ -1,3 +1,7 @@
+// Copyright (c) KwanJunWen
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
 package owsms_test
 
 import (
